5a1c745a74e75af0431ff89e4234eb6184fb6eef
Max <max.randhahn@yahoo.de>
2019-12-07 21:49:11 +0100

    Added Logic.py

---
+++ logical_operations/logic.py
@@ -0,0 +1,30 @@
+def xor_binString(binString, binString_, bit_size=8):
+    tmp = []
+    for bit in range(0, bit_size):
+        sbits = int(binString[bit])
+        sbits_ = int(binString_[bit])
+        if sbits == 1 and sbits_ == 1:
+            tmp.append(str(0))
+        elif sbits == 1 and sbits_ == 0 or sbits == 0 and sbits_ == 1:
+            tmp.append(str(1))
+        elif sbits == 0 and sbits_ == 0:
+            tmp.append(str(0))
+    tmp = "".join(tmp)
+    return tmp
+
+
+def convert_hex_to_bin_string(hexVal, out_len=8):
+    return f"{hexVal:0>{out_len}b}"
+
+
+def shift_bit(binString, direction, val_to_shift):
+    tmp = []
+    if direction == "<<":  # L_Shift
+        tmp.append(binString[val_to_shift:])
+        [tmp.append("0") for i in range(0, val_to_shift)]
+    elif direction == ">>":  # R_Shift
+        x = binString[:(binString.__len__() - val_to_shift)]
+        [tmp.append("0") for i in range(0, val_to_shift)]
+        tmp.append(x)
+    tmp = ''.join(tmp)
+    return tmp
