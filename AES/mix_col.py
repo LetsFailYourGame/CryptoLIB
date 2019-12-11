e110a5723e8fc27f167b95bb091d2728dd756a10
Max <max.randhahn@yahoo.de>
2019-12-12 00:28:55 +0100

    Simplified functions in AES/utils/mix_col.py and AES/utils/functions.py Added calc example for mix_col

--- AES/mix_col.py
+++ AES/mix_col.py
@@ -1,14 +1,6 @@
-import logical_operations.logic as logic
-from AES.utils.functions import convert_4bytes_to_bin_string_matrix, mix_col
+# Example for calculating mix_col
+import AES.utils.functions as aes
 
-input_bytes = [
-    0x23, 0x24, 0x34, 0xF8
-]
+input_bytes = [0x1E, 0xEA, 0x24, 0xB3]
 
-b_matrix = convert_4bytes_to_bin_string_matrix(input_bytes)
-tmp = mix_col(b_matrix)
-
-for i in range(0, 15, 4):
-    x1 = logic.xor_binString(tmp[i], tmp[i + 1])
-    x2 = logic.xor_binString(tmp[i + 2], tmp[i + 3])
-    print(logic.xor_binString(x1, x2))
+print(aes.mix_col_get_keys(aes.mix_col(input_bytes)))
--- AES/utils/functions.py
+++ AES/utils/functions.py
@@ -3,32 +3,37 @@ import logical_operations.logic as logic
 mix_col_matrix = [[2, 3, 1, 1], [1, 2, 3, 1], [1, 1, 2, 3], [3, 1, 1, 2]]
 
 
-def convert_4bytes_to_bin_string_matrix(byte_matrix: hex) -> list:
-    tmp_matrix = [
-        logic.convert_hex_to_bin_string(byte_matrix[0]),
-        logic.convert_hex_to_bin_string(byte_matrix[1]),
-        logic.convert_hex_to_bin_string(byte_matrix[2]),
-        logic.convert_hex_to_bin_string(byte_matrix[3])
-    ]
-    return tmp_matrix
+def mix_col_get_keys(mix_col_list: list):
+    k = []
+    for i in range(0, 15, 4):
+        x1 = logic.xor_binString(mix_col_list[i], mix_col_list[i + 1])
+        x2 = logic.xor_binString(mix_col_list[i + 2], mix_col_list[i + 3])
+        k.append(logic.xor_binString(x1, x2))
+    return k
 
 
-def mix_col(bin_matrix: list) -> list:
+def mix_col(hex_input_list: list) -> list:
     y = []
     px = "00011011"
+    tmp_matrix = [
+        logic.convert_hex_to_bin_string(hex_input_list[0]),
+        logic.convert_hex_to_bin_string(hex_input_list[1]),
+        logic.convert_hex_to_bin_string(hex_input_list[2]),
+        logic.convert_hex_to_bin_string(hex_input_list[3])
+    ]
     for row in range(0, mix_col_matrix.__len__()):
         for column in range(0, mix_col_matrix.__len__()):
-            shift = logic.shift_bit(bin_matrix[column], "<<", 1)
-            xo = logic.xor_binString(shift, bin_matrix[column])
+            shift = logic.shift_bit(tmp_matrix[column], "<<", 1)
+            xo = logic.xor_binString(shift, tmp_matrix[column])
             if mix_col_matrix[row][column] == 1:
-                y.append(bin_matrix[column])
+                y.append(tmp_matrix[column])
             elif mix_col_matrix[row][column] == 2:
-                if bin_matrix[column][0] == "1":
+                if tmp_matrix[column][0] == "1":
                     y.append(logic.xor_binString(shift, px))
                 else:
                     y.append(shift)
             elif mix_col_matrix[row][column] == 3:
-                if bin_matrix[column][0] == "1":
+                if tmp_matrix[column][0] == "1":
                     red = logic.xor_binString(xo, px)
                     y.append(red)
                 else:
