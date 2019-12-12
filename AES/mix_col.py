2c3eac6c0b2edd24da26b98bb9bcb5f367f78221
Max <max.randhahn@yahoo.de>
2019-12-12 22:38:24 +0100

    Simplified Key finding Example

--- AES/mix_col.py
+++ AES/mix_col.py
@@ -10,24 +10,12 @@ cd = [0x23, 0x24, 0x34, 0xF8]
 
 round_key_1 = convert_hex_to_bin_string(0xF5135F3F89A87757203ED1B212E003F9, 128)
 
-c_combine = []
 
-x = aes.mix_col_get_keys(aes.mix_col(ca))
-y = aes.mix_col_get_keys(aes.mix_col(cb))
-z = aes.mix_col_get_keys(aes.mix_col(cc))
-a = aes.mix_col_get_keys(aes.mix_col(cd))
+all_c_combine = [''.join(aes.mix_col_get_keys(aes.mix_col(ca))), ''.join(aes.mix_col_get_keys(aes.mix_col(cb))),
+                 ''.join(aes.mix_col_get_keys(aes.mix_col(cc))), ''.join(aes.mix_col_get_keys(aes.mix_col(cd)))]
 
-x = ''.join(x)
-y = ''.join(y)
-z = ''.join(z)
-a = ''.join(a)
+all_c_combine = ''.join(all_c_combine)
 
-c_combine.append(x)
-c_combine.append(y)
-c_combine.append(z)
-c_combine.append(a)
-c_combine = ''.join(c_combine)
-
-xor_round_key = xor(c_combine, round_key_1, 128)
+xor_round_key = xor(all_c_combine, round_key_1, 128)
 
 print(f"Key: {xor_round_key}")
